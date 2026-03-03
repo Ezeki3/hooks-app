219925c2c6d3b55a0ced20aa43fddf7a2ae42c9f
Ezequiel Lino <sonisombra@gmail.com>
2026-03-03 16:55:16 -0600

    useEffect - disparar efectos secundarios

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 
-import { TrafficLight } from './01-useState/TrafficLight'
+// import { TrafficLight } from './01-useState/TrafficLight'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+
+    <TrafficLightWithEffect />
+
   </StrictMode>,
 )
