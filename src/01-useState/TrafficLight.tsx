a877c0917d13eaa7425d0d376bdb8e36f15e327a
Ezequiel Lino <sonisombra@gmail.com>
2026-03-03 16:31:13 -0600

    tipado estricto en useState

--- src/01-useState/TrafficLight.tsx
+++ src/01-useState/TrafficLight.tsx
@@ -6,11 +6,14 @@ const colors = {
   green: 'bg-green-500 animate-pulse',
 }
 
+// type TrafficLightColor = 'red' | 'yellow' | 'green';
+type TrafficLightColor = keyof typeof colors;
+
 export const TrafficLight = () => {
 
-  const [light, setLight] = useState('red');
+  const [light, setLight] = useState<TrafficLightColor>('red');
 
-  const handleColorChange = (color: string) => {
+  const handleColorChange = (color: TrafficLightColor) => {
     setLight((prev) => {
       console.log({ prev });
       return color
