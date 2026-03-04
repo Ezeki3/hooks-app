cf1b01d4f63045ec96ff400a4a284f50184109b9
Ezequiel Lino <sonisombra@gmail.com>
2026-03-04 17:30:30 -0600

    conexion de varios hooks

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
+import { PokemonPage } from './03-examples/PokemonPage'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
-import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 
 // import { TrafficLight } from './01-useState/TrafficLight'
 
@@ -15,7 +16,9 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
 
     {/* <TrafficLightWithEffect /> */}
-    <TrafficLightWithHook />
+    {/* <TrafficLightWithHook /> */}
+
+    <PokemonPage />
 
   </StrictMode>,
 )
