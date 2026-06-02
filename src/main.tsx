89dc6d32212b82cae87e5b8065cb76560540846d
Ezequiel Lino <sonisombra@gmail.com>
2026-06-02 14:00:32 -0600

    useOptimistic

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 // import { MemoHook } from './06-memos/MemoHook'
-import { MemoCounter } from './06-memos/MemoCounter'
+// import { MemoCounter } from './06-memos/MemoCounter'
+import { InstagromApp } from './07-useOptimistic/InstagromApp'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
@@ -32,6 +33,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <ScrambleWords /> */}
 
     {/* <MemoHook /> */}
-    <MemoCounter />
+    {/* <MemoCounter /> */}
+
+    <InstagromApp />
   </StrictMode>,
 )
